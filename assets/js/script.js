e7f7ab7ddcf3fcdc3ae36d2bc110ea43af256a06
burtk33 <burtk33@gmail.com>
2021-01-19 19:24:35 -0500

    added functions to checkAnswers and dynamically change text content

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,24 @@ var timerElement = document.querySelector("#timer");
 var startButton = document.querySelector("#start-button");
 var startTitle = document.querySelector("h3");
 var questionBlock = document.querySelector("#question");
-var answerButtons = document.querySelector("#answer-buttons")
+var answerButtons = document.querySelector("#answer-buttons");
+var selectionMsg = document.querySelector("#alert-msg");
 
 var timer;
 var timerCount;
 var complete = false;
 var questionCount = 0;
+var score = 0;
+
 
 
 // Question array of objects
 var questionArray = [
     { question: "question 1", possibleAnswers: ["incorrect", "incorrect", "incorrect", "correct"], correct: "correct" },
-    { question: "question 2", possibleAnswers: [], correct: "" },
-    { question: "question 3", possibleAnswers: [], correct: "" },
-    { question: "question 4", possibleAnswers: [], correct: "" }];
+    { question: "question 2", possibleAnswers: ["correct", "incorrect", "incorrect", "incorrect"], correct: "correct" },
+    { question: "question 3", possibleAnswers: ["incorrect", "incorrect", "correct", "incorrect"], correct: "correct" },
+    { question: "question 4", possibleAnswers: ["incorrect", "correct", "incorrect", "incorrect"], correct: "correct" }];
+
 
 
 // The startGame function is called when the start button is clicked
@@ -24,29 +28,55 @@ function startGame() {
     timerCount = 75;
     startTimer();
     startTitle.textContent = "";
+    createButtons();
     renderQuestion();
+    startButton.remove();
+}
+
+function createButtons() {
+    for (i = 0; i < 4; i++) {
+        var newButton = document.createElement("button");
+        newButton.setAttribute("class", "btn btn-primary");
+        newButton.setAttribute("type", "button");
+        newButton.setAttribute("id", i);
+        newButton.addEventListener("click", checkAnswer);
+        answerButtons.appendChild(newButton);
+    }
 }
 
 
 function renderQuestion() {
     var q = questionArray[questionCount];
-    questionBlock.innerHTML = q.question;
-    for (i = 0; i < 4; i++) {
-        var newButton = document.createElement("button");
-        newButton.setAttribute("class", "btn btn-primary");
-        newButton.setAttribute("type", "button");
-        newButton.addEventListener("click", checkAnswer);
-        newButton.innerHTML = q.possibleAnswers[i];
-        answerButtons.appendChild(newButton);
+    questionBlock.textContent = q.question;
+    for (i = 0; i < q.possibleAnswers.length; i++) {
+        var buttonRender = document.getElementById(i);
+        buttonRender.textContent = q.possibleAnswers[i];
     }
+
 };
 
-//answerButtons.addEventListener("click", checkAnswer);
 
-function checkAnswer(){
+function checkAnswer() {
     var buttonClicked = this.textContent;
+    var q = questionArray[questionCount];
     console.log(buttonClicked);
-}
+        if (buttonClicked === q.correct && timerCount > 0) {
+            selectionMsg.textContent = "Correct!";
+            questionCount++;
+            console.log(questionCount);
+            if(questionCount<questionArray.length){
+                renderQuestion();
+            }
+            else{
+                endGame();
+            }
+            
+        }
+        else {
+            selectionMsg.textContent = "Incorrect!";
+            timerCount -= 10;
+        }
+};
 
 
 // Function to start the timer
@@ -65,6 +95,13 @@ function startTimer() {
             endGame();
         }
     }, 1000);
+}
+
+function endGame(){
+    score=timerCount;
+    answerButtons.remove();
+    questionBlock.textContent="";
+    var initials= prompt("Your score is "+score);
 }
 
 
